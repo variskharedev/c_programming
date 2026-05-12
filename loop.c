2dcbe6c389b1f1b04b4b2001ae822ce944281d40
variskhare <variskhare@gmail.com>
2026-05-12 22:40:22 +0530

    looping concept

---
+++ loop.c
@@ -0,0 +1,15 @@
+# include <stdio.h>
+
+// here is %d is format spcifier for integer i variable which loop runs 6 times as starts fro 0 to <=5
+
+int main(){
+ 
+    for(int i = 0;i<=5; i++){
+
+        printf("%d",i);
+
+    }
+   
+    return 0;
+
+}
