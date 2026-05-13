d10a5feeb54cd328dbb8aae518360e6670681cb2
variskhare <variskhare@gmail.com>
2026-05-13 22:42:31 +0530

    Odd Even number Finding

---
+++ even.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int  main(){
+
+    int e;
+    printf("Enter number\n");
+
+    scanf("%d",&e);
+
+    if(e % 2 == 0 ){
+
+        printf(" %d is even",e);
+    }
+
+    else 
+    printf("Odd number");
+       
+
+
+    
+
+    return 0;
+}
